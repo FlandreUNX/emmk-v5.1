fdb555a29b36b8b6eddf7b4da16ca72f70c170b6
FlandreUNX <FlandreUNX@outlook.com>
2025-08-13 11:12:10 +0800

    feat(driver/swm181): Support kduart irq send

--- hal/swm181x/driver/kduart.c
+++ hal/swm181x/driver/kduart.c
@@ -21,62 +21,8 @@
  * @addtogroup Hal support
  * @note none
  */
- 
+
 /*@{*/
-
-static void mutexNew(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    const osMutexAttr_t mutex_attr = {
-        .attr_bits = osMutexRobust | osMutexPrioInherit
-    };
-    
-    kd->_va->writeMutex = osMutexNew(&mutex_attr);
-    ASSERT(kd->_va->writeMutex != NULL);
-    
-    kd->_va->recvMutex = osMutexNew(&mutex_attr);
-    ASSERT(kd->_va->recvMutex != NULL);
-#endif
-}
-
-
-static void mutexDelete(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    osMutexDelete(kd->_va->writeMutex);
-    kd->_va->writeMutex = NULL;
-    
-    osMutexDelete(kd->_va->recvMutex);
-    kd->_va->recvMutex = NULL;
-#endif
-}
-
-
-__STATIC_FORCEINLINE void mutexWriteLock(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    osMutexAcquire(kd->_va->writeMutex, osWaitForever);
-#endif
-}
-
-
-__STATIC_FORCEINLINE void mutexWriteUnlock(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    osMutexRelease(kd->_va->writeMutex);
-#endif
-}
-
-
-__STATIC_FORCEINLINE void mutexRecvLock(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    osMutexAcquire(kd->_va->recvMutex, osWaitForever);
-#endif
-}
-
-
-__STATIC_FORCEINLINE void mutexRecvUnlock(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    osMutexRelease(kd->_va->recvMutex);
-#endif
-}
-
 
 __STATIC_FORCEINLINE void threadYield(void) {
 #if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
@@ -89,13 +35,13 @@ __STATIC_FORCEINLINE void threadYield(void) {
 
 static int32_t halBlockTxIsFull(kduart_t *kd) {
     qSTimer_t tmcd;
-    
+
     qSTimer_Set(&tmcd, 100);
     while ((kd->_config.uart.uart->CTRL & UART_CTRL_TXFF_Msk)
-            && (!qSTimer_Expired(&tmcd))) {
+           && (!qSTimer_Expired(&tmcd))) {
         threadYield();
     }
-            
+
     if (qSTimer_Expired(&tmcd)) {
         return -1;
     } else {
@@ -106,13 +52,13 @@ static int32_t halBlockTxIsFull(kduart_t *kd) {
 
 static int32_t halBlockTxCompleted(kduart_t *kd) {
     qSTimer_t tmcd;
-    
+
     qSTimer_Set(&tmcd, 100);
     while (!(kd->_config.uart.uart->CTRL & 0x01u)
-            && (!qSTimer_Expired(&tmcd))) {
+           && (!qSTimer_Expired(&tmcd))) {
         threadYield();
     }
-            
+
     if (qSTimer_Expired(&tmcd)) {
         return -1;
     } else {
@@ -126,7 +72,7 @@ static int32_t halBlockTxCompleted(kduart_t *kd) {
  * @addtogroup Public functions support
  * @note none
  */
- 
+
 /*@{*/
 
 int32_t kduart_init(kduart_t *kd) {
@@ -135,126 +81,109 @@ int32_t kduart_init(kduart_t *kd) {
     if (emmkDriver_initRefsCountUp(&kd->_va->initRefs) != 0) {
         return -1;
     }
-    
+
     kd->_base.baseEnable(kd);
-    
+
     UART_Init(kd->_config.uart.uart, &kd->_config.uart.init);
 
 #if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
     kd->_va->flag = osEventFlagsNew(NULL);
     ASSERT(kd->_va->flag != NULL);
-    
-    mutexNew(kd);
 #else
     kd->_va->flag._ = 0;
     kd->_va->flag.isSendCompleted = 1;
 #endif
-    
-    if (kd->_buffer.recvLwrb != NULL) {
-        qBSBuffer_Setup(kd->_buffer.recvLwrb, kd->_buffer.recvBuffer, kd->_buffer.recvBufferSize);
+
+    if (kd->buffer.recvLwrb != NULL) {
+        qBSBuffer_Setup(kd->buffer.recvLwrb, kd->buffer.recvBuffer, kd->buffer.recvBufferSize);
+    }
+    if (kd->buffer.writeLwrb != NULL) {
+        qBSBuffer_Setup(kd->buffer.writeLwrb, kd->buffer.writeBuffer, kd->buffer.writeBufferSize);
     }
     kduart_flush(kd);
-    
+
     return 0;
 }
 
 int32_t kduart_finalize(kduart_t *kd) {
     ASSERT(kd != NULL);
-    
+
     if (emmkDriver_initRefsCountDown(&kd->_va->initRefs) != 0) {
         return -1;
     }
-    
+
     if (kd->_va->initRefsPower != 0) {
         kd->_va->initRefsPower = 1;
         kduart_powerDown(kd);
     }
-    
+
     kd->_base.baseDisable(kd);
 
 #if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
     osEventFlagsDelete(kd->_va->flag);
     kd->_va->flag = NULL;
-    
-    mutexDelete(kd);
 #endif
-    
+
     return 0;
 }
 
 int32_t kduart_powerUp(kduart_t *kd) {
     ASSERT(kd != NULL);
-    
-    int32_t rc = -1;
-    
+
     if (emmkDriver_initRefsCountUp(&kd->_va->initRefsPower) != 0) {
         return -1;
     }
-    
-    mutexWriteLock(kd);
-    mutexRecvLock(kd);
 
     if (kd->_config.pin.tx.gpio != NULL) {
-        PORT_Init(kd->_config.pin.tx.gpio->_config.base.port, 
-            kd->_config.pin.tx.gpio->_config.base.number,
-            kd->_config.pin.tx.af, 1);
+        PORT_Init(kd->_config.pin.tx.gpio->_config.base.port,
+                  kd->_config.pin.tx.gpio->_config.base.number,
+                  kd->_config.pin.tx.af, 1);
     }
-    
+
     if (kd->_config.pin.rx.gpio != NULL) {
-        PORT_Init(kd->_config.pin.rx.gpio->_config.base.port, 
-            kd->_config.pin.rx.gpio->_config.base.number,
-            kd->_config.pin.rx.af, 1);
+        PORT_Init(kd->_config.pin.rx.gpio->_config.base.port,
+                  kd->_config.pin.rx.gpio->_config.base.number,
+                  kd->_config.pin.rx.af, 1);
     }
-    
-    if (kd->_buffer.recvLwrb != NULL) {
-        qBSBuffer_Setup(kd->_buffer.recvLwrb, kd->_buffer.recvBuffer, kd->_buffer.recvBufferSize);
+
+    if (kd->buffer.recvLwrb != NULL) {
+        qBSBuffer_Setup(kd->buffer.recvLwrb, kd->buffer.recvBuffer, kd->buffer.recvBufferSize);
     }
+    if (kd->buffer.writeLwrb != NULL) {
+        qBSBuffer_Setup(kd->buffer.writeLwrb, kd->buffer.writeBuffer, kd->buffer.writeBufferSize);
+    }
+
+    UART_INTTXThresholdDis(kd->_config.uart.uart);
     UART_Open(kd->_config.uart.uart);
-    
-    rc = 0;
-    
-l_exit:
-    mutexWriteUnlock(kd);
-    mutexRecvUnlock(kd);
-    
-    return rc;
+
+    return 0;
 }
 
 int32_t kduart_powerDown(kduart_t *kd) {
     ASSERT(kd != NULL);
-    
-    int32_t rc = -1;
-    
+
     if (emmkDriver_initRefsCountDown(&kd->_va->initRefsPower) != 0) {
         return -1;
     }
-    
-    mutexWriteLock(kd);
-    mutexRecvLock(kd);
-    
+
     if (kd->_config.pin.tx.gpio != NULL) {
-        PORT_Init(kd->_config.pin.tx.gpio->_config.base.port, 
-            kd->_config.pin.tx.gpio->_config.base.number,
-            0, 0);
+        PORT_Init(kd->_config.pin.tx.gpio->_config.base.port,
+                  kd->_config.pin.tx.gpio->_config.base.number,
+                  0, 0);
         kdgpio_powerDown(kd->_config.pin.tx.gpio);
     }
-    
+
     if (kd->_config.pin.rx.gpio != NULL) {
-        PORT_Init(kd->_config.pin.rx.gpio->_config.base.port, 
-            kd->_config.pin.rx.gpio->_config.base.number,
-            0, 0);
+        PORT_Init(kd->_config.pin.rx.gpio->_config.base.port,
+                  kd->_config.pin.rx.gpio->_config.base.number,
+                  0, 0);
         kdgpio_powerDown(kd->_config.pin.rx.gpio);
     }
-    
+
+    UART_INTTXThresholdDis(kd->_config.uart.uart);
     UART_Close(kd->_config.uart.uart);
-    
-    rc = 0;
-    
-l_exit:
-    mutexWriteUnlock(kd);
-    mutexRecvUnlock(kd);
-    
-    return rc;
+
+    return 0;
 }
 
 
@@ -262,12 +191,13 @@ int32_t kduart_sends(kduart_t *kd, const void *data, uint32_t size, uint32_t tim
 #if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
     int32_t len;
     uint32_t flag;
-    
+
     if (size == 0) {
         return -1;
     }
-    
-    if ((osEventFlagsGet(kd->_va->flag) & (UART_FLAG_SEND_DMA_COMPLETE | UART_FLAG_SEND_IDLE)) == (UART_FLAG_SEND_DMA_COMPLETE | UART_FLAG_SEND_IDLE)) {
+
+    if ((osEventFlagsGet(kd->_va->flag) & (UART_FLAG_SEND_DMA_COMPLETE | UART_FLAG_SEND_IDLE)) == (
+            UART_FLAG_SEND_DMA_COMPLETE | UART_FLAG_SEND_IDLE)) {
         mutexWriteUnlock(kd);
     }
 
@@ -276,7 +206,7 @@ int32_t kduart_sends(kduart_t *kd, const void *data, uint32_t size, uint32_t tim
     }
 
     osEventFlagsClear(kd->_va->flag, UART_FLAG_SEND_DMA_COMPLETE | UART_FLAG_SEND_IDLE);
-    
+
     for (uint32_t i = 0; i < size; i++) {
         if (halBlockTxIsFull(kd) != 0) {
             UART_Close(kd->_config.uart.uart);
@@ -287,7 +217,7 @@ int32_t kduart_sends(kduart_t *kd, const void *data, uint32_t size, uint32_t tim
 
         UART_WriteByte(kd->_config.uart.uart, ((uint8_t *) data)[i]);
     }
-    
+
     if (halBlockTxCompleted(kd) != 0) {
         UART_Close(kd->_config.uart.uart);
         UART_Open(kd->_config.uart.uart);
@@ -299,26 +229,51 @@ int32_t kduart_sends(kduart_t *kd, const void *data, uint32_t size, uint32_t tim
     mutexWriteUnlock(kd);
     return size;
 #else
-    for (register uint32_t i = 0; i < size; i++) {
-        if (halBlockTxIsFull(kd) != 0) {
-            UART_Close(kd->_config.uart.uart);
-            UART_Open(kd->_config.uart.uart);
+    if (size == 0) {
+        return -1;
+    }
+    qSTimer_t wait;
+
+
+    if (kd->_va->flag.isSendCompleted == 0) {
+        if (timeout == 0) {
             return -1;
         }
-
-        UART_WriteByte(kd->_config.uart.uart, ((uint8_t *) data)[i]);
+        qSTimer_Set(&wait, timeout);
+        while (!qSTimer_Expired(&wait)) {
+            if (kd->_va->flag.isSendCompleted) {
+                break;
+            }
+        }
+        if (qSTimer_Expired(&wait)) {
+            return -1;
+        }
     }
+    kd->_va->flag.isSendCompleted = 0;
 
-    while (UART_IsTXBusy(kd->_config.uart.uart)) {
-        threadYield();
+    if (size != 1
+        && kd->buffer.writeBufferSize != 0
+        && size <= kd->buffer.writeBufferSize) {
+        for (uint32_t i = 0; i < size; i++) {
+            qBSBuffer_Put(kd->buffer.writeLwrb, ((uint8_t *) data)[i]);
+        }
+        UART_INTTXThresholdEn(kd->_config.uart.uart);
+    } else {
+        for (register uint32_t i = 0; i < size; i++) {
+            if (halBlockTxIsFull(kd) != 0) {
+                UART_Close(kd->_config.uart.uart);
+                UART_Open(kd->_config.uart.uart);
+                return -1;
+            }
+            UART_WriteByte(kd->_config.uart.uart, ((uint8_t *) data)[i]);
+        }
+        while (UART_IsTXBusy(kd->_config.uart.uart)) {
+            threadYield();
+        }
+        kd->_va->flag.isSendCompleted = 1;
     }
-
     return size;
 #endif
-}
-
-int32_t kduart_tsends9(kduart_t *kd, uint16_t *data, uint32_t size, uint32_t timeout) {
-    return size;
 }
 
 
@@ -326,15 +281,15 @@ int32_t kduart_recvs(kduart_t *kd, void *data, uint32_t expect_size, uint32_t *r
 #if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
     uint32_t flag;
     uint32_t rcv_len;
-    
+
     if (expect_size == 0) {
         if (recv_size != NULL) {
             *recv_size = 0;
         }
-        
+
         return 0;
-    }  
-    
+    }
+
     mutexWriteLock(kd);
 
     if (timeout != 0) {
@@ -343,7 +298,7 @@ int32_t kduart_recvs(kduart_t *kd, void *data, uint32_t expect_size, uint32_t *r
             if (recv_size != NULL) {
                 *recv_size = 0;
             }
-            
+
             mutexRecvUnlock(kd);
             return -1;
         }
@@ -353,27 +308,27 @@ int32_t kduart_recvs(kduart_t *kd, void *data, uint32_t expect_size, uint32_t *r
             if (recv_size != NULL) {
                 *recv_size = 0;
             }
-            
+
             mutexRecvUnlock(kd);
             return -1;
         }
-        
+
         osEventFlagsClear(kd->_va->flag, UART_FLAG_RECV_COMPLETE);
     }
 
-    if (klwrb_getFull(kd->_buffer.recvLwrb) == 0) {
+    if (klwrb_getFull(kd->buffer.recvLwrb) == 0) {
         if (recv_size != NULL) {
             *recv_size = 0;
         }
-        
+
         mutexRecvUnlock(kd);
         return 0;
     }
-    
+
     if (recv_size != NULL) {
-        *recv_size = klwrb_read(kd->_buffer.recvLwrb, data, expect_size);
+        *recv_size = klwrb_read(kd->buffer.recvLwrb, data, expect_size);
     } else {
-        klwrb_read(kd->_buffer.recvLwrb, data, expect_size);
+        klwrb_read(kd->buffer.recvLwrb, data, expect_size);
     }
 
     mutexRecvUnlock(kd);
@@ -385,11 +340,11 @@ int32_t kduart_recvs(kduart_t *kd, void *data, uint32_t expect_size, uint32_t *r
         }
         return -1;
     }
-            
-    uint32_t buffCount = qBSBuffer_Count(kd->_buffer.recvLwrb);
+
+    uint32_t buffCount = qBSBuffer_Count(kd->buffer.recvLwrb);
     if (buffCount == 0) {
         kd->_va->flag.isRecvCompleted = 0;
-    
+
         if (recv_size != NULL) {
             *recv_size = 0;
         }
@@ -397,30 +352,30 @@ int32_t kduart_recvs(kduart_t *kd, void *data, uint32_t expect_size, uint32_t *r
     }
     buffCount = buffCount <= expect_size ? buffCount : expect_size;
     if (recv_size != NULL) {
-        if (qBSBuffer_Read(kd->_buffer.recvLwrb, data, buffCount) != qTrue) {
+        if (qBSBuffer_Read(kd->buffer.recvLwrb, data, buffCount) != qTrue) {
             *recv_size = 0;
         } else {
             *recv_size = buffCount;
         }
     } else {
-        qBSBuffer_Read(kd->_buffer.recvLwrb, data, buffCount);
+        qBSBuffer_Read(kd->buffer.recvLwrb, data, buffCount);
     }
-    
-    if (qBSBuffer_Count(kd->_buffer.recvLwrb) == 0) {
+
+    if (qBSBuffer_Count(kd->buffer.recvLwrb) == 0) {
         kd->_va->flag.isRecvCompleted = 0;
     }
-    
+
     return 0;
 #endif
 }
 
 
 int32_t kduart_flush(kduart_t *kd) {
-    mutexWriteLock(kd);
-    mutexRecvLock(kd);
-    
-    if (kd->_buffer.recvLwrb != NULL) {
-        qBSBuffer_Setup(kd->_buffer.recvLwrb, kd->_buffer.recvBuffer, kd->_buffer.recvBufferSize);
+    if (kd->buffer.recvLwrb != NULL) {
+        qBSBuffer_Setup(kd->buffer.recvLwrb, kd->buffer.recvBuffer, kd->buffer.recvBufferSize);
+    }
+    if (kd->buffer.writeLwrb != NULL) {
+        qBSBuffer_Setup(kd->buffer.writeLwrb, kd->buffer.writeBuffer, kd->buffer.writeBufferSize);
     }
 
 #if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
@@ -430,9 +385,7 @@ int32_t kduart_flush(kduart_t *kd) {
     kd->_va->flag._ = 0;
     kd->_va->flag.isSendCompleted = 1;
 #endif
-    mutexWriteUnlock(kd);
-    mutexRecvUnlock(kd);
-    
+
     return 0;
 }
 
@@ -442,46 +395,57 @@ int32_t kduart_hasRecvData(kduart_t *kd) {
     int32_t flag = 0;
 
     flag = osEventFlagsGet(kd->_va->flag);
-    
+
     if ((flag > 0) && (flag & (UART_FLAG_RECV_COMPLETE))) {
-        return klwrb_getFull(kd->_buffer.recvLwrb);
+        return klwrb_getFull(kd->buffer.recvLwrb);
     }
 #else
     if ((kd->_va->flag.isRecvCompleted) && !kd->_va->flag.isRecving) {
-        return qBSBuffer_Count(kd->_buffer.recvLwrb);
+        return qBSBuffer_Count(kd->buffer.recvLwrb);
     }
 #endif
     return 0;
 }
 
-int32_t kduart_tisSendIdle(kduart_t *kd) {
-#if EMMK_FULL_RTOS_SUPPORT == EMMK_FULL_RTOS_RTX5
-    int32_t flag = 0;
-    
-    flag = osEventFlagsGet(kd->_va->flag);
-   
-    return (flag > 0) && (flag & (UART_FLAG_SEND_DMA_COMPLETE | UART_FLAG_SEND_IDLE));
-#else
-    return kd->_va->flag.isSendCompleted;
-#endif
+
+int32_t kduart_isSendIdle(kduart_t *kd, uint32_t wait) {
+    if (kd->_va->flag.isSendCompleted) {
+        return true;
+    }
+    if (wait == 0) {
+        return false;
+    }
+    qSTimer_t waitTimer;
+    qSTimer_Set(&waitTimer, wait);
+    while (!qSTimer_Expired(&waitTimer)) {
+        if (kd->_va->flag.isSendCompleted) {
+            return true;
+        }
+    }
+    return true;
 }
 
 void kduart_updateBaudRate(kduart_t *kd, uint32_t bd) {
-    mutexWriteLock(kd);
-    mutexRecvLock(kd);
-    
     uint32_t isWorking = kd->_config.uart.uart->CTRL & 0x200u;
 
     UART_Close(kd->_config.uart.uart);
-    
+
     UART_SetBaudrate(kd->_config.uart.uart, bd);
-    
+
     if (isWorking) {
         UART_Open(kd->_config.uart.uart);
     }
+}
 
-    mutexWriteUnlock(kd);
-    mutexRecvUnlock(kd);
+
+bool kduart_sendBuffingVerify(kduart_t *kd, uint32_t dataSize) {
+    if (kd->buffer.writeBufferSize == 0) {
+        return false;
+    }
+    if (dataSize > kd->buffer.writeBufferSize) {
+        return false;
+    }
+    return true;
 }
 
 /*@}*/
